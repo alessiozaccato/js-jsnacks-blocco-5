5f5c52859ff1a6ba5a68db53f8a1c576e6985f2f
alessiozaccato <alessiozaccato@gmail.com>
2025-01-27 12:46:57 +0100

    ADD:fifth exercise

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,9 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const numsEven = nums.filter((element) => {
+    return (element % 2 === 0) ? (element) : (null); //first time that i will use the ternary operator
+})
+
+console.log(numsEven);
