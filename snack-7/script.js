9c0a3be99c736bf94e58a74c5f9d3f9c62ea009e
alessiozaccato <alessiozaccato@gmail.com>
2025-01-27 14:22:17 +0100

    ADD:exercise 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const sutdentId2 = students.find((element) => {
+  return element.id == 2;
+})
+
+console.log(sutdentId2)//print on console 
